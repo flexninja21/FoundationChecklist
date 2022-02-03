9abfacd069f0040560bee36c6929f995602592a8
Edem Afflu <44079034+flexninja21@users.noreply.github.com>
2022-02-03 07:28:45 +0000

    Add files via upload

---
+++ SolidPrinciples/src/main/java/SingleResponsibility.java
@@ -0,0 +1,13 @@
+public class SingleResponsibility {
+    /*design principles encourage us to create more maintainable,
+    understandable, and flexible software. Consequently,
+    as our applications grow in size, we can reduce their complexity
+    and save ourselves a lot of headaches further down the road!*/
+
+    //https://www.youtube.com/watch?v=BM_lSZPMClo
+    //https://medium.com/@javatechie/solid-design-principle-java-ae96a48db97
+    //https://www.baeldung.com/solid-principles
+    //a class must only have a single job/responsibility/functionality and do it really well
+
+
+}
